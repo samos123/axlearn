10914fc3249f10400283791a463e0b497cf08192
Sam Stoelinga <sammiestoel@gmail.com>
2024-08-06 14:43:23 -0700

    gbs / 2

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -151,7 +151,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=3e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=int(train_batch_size / 4),
+            train_batch_size=train_batch_size,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(data=-1, fsdp=8),
             mesh_rules=(
@@ -191,7 +191,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=int(train_batch_size / 4),
+            train_batch_size=train_batch_size,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(
@@ -214,6 +214,7 @@ def get_trainer_kwargs(
         max_step=trainer_kwargs["max_step"],
         **trainer_kwargs.pop("learner_kwargs"),
     )
+    trainer_kwargs["train_batch_size"] = int(trainer_kwargs["train_batch_size"] / 2)
     # Remove this before merging, just to make it stop quickly
     trainer_kwargs["max_step"] = 120
     # pylint: enable=use-dict-literal

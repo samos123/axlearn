1fc7ff8fb2ceae9a4412f41da6687bbc09636aff
Sam Stoelinga <sammiestoel@gmail.com>
2024-09-24 13:33:28 -0700

    Revert "disable all xla flags"
    This reverts commit 7e563a29581bbfdf818880c6372f16ffa151b5a1.

--- axlearn/cloud/gcp/job.py
+++ axlearn/cloud/gcp/job.py
@@ -780,22 +780,22 @@ class GPUGKEJob(GKEJob):
         env_vars["NUM_PROCESSES"] = f"{cfg.accelerator.num_replicas}"
 
         default_xla_flags = [
-            # "--xla_gpu_enable_latency_hiding_scheduler=true",
-            # "--xla_gpu_enable_triton_gemm=false",
-            # "--xla_gpu_enable_highest_priority_async_stream=true",
-            # # Increase combine threshold to 1GB for improved performance.
-            # # A3 and TCPX performs bad with smaller message sizes.
-            # "--xla_gpu_all_reduce_combine_threshold_bytes=134217728",
-            # "--xla_gpu_all_gather_combine_threshold_bytes=1073741824",
-            # "--xla_gpu_reduce_scatter_combine_threshold_bytes=33554432",
-            # "--xla_gpu_enable_pipelined_all_gather=true",
-            # "--xla_gpu_enable_pipelined_reduce_scatter=true",
-            # "--xla_gpu_enable_pipelined_all_reduce=true",
-            # "--xla_gpu_enable_while_loop_double_buffering=true",
-            # "--xla_gpu_enable_triton_softmax_fusion=false",
-            # "--xla_gpu_enable_all_gather_combine_by_dim=false",
-            # "--xla_gpu_enable_reduce_scatter_combine_by_dim=false",
-            # "--xla_disable_hlo_passes=rematerialization",
+            "--xla_gpu_enable_latency_hiding_scheduler=true",
+            "--xla_gpu_enable_triton_gemm=false",
+            "--xla_gpu_enable_highest_priority_async_stream=true",
+            # Increase combine threshold to 1GB for improved performance.
+            # A3 and TCPX performs bad with smaller message sizes.
+            "--xla_gpu_all_reduce_combine_threshold_bytes=134217728",
+            "--xla_gpu_all_gather_combine_threshold_bytes=1073741824",
+            "--xla_gpu_reduce_scatter_combine_threshold_bytes=33554432",
+            "--xla_gpu_enable_pipelined_all_gather=true",
+            "--xla_gpu_enable_pipelined_reduce_scatter=true",
+            "--xla_gpu_enable_pipelined_all_reduce=true",
+            "--xla_gpu_enable_while_loop_double_buffering=true",
+            "--xla_gpu_enable_triton_softmax_fusion=false",
+            "--xla_gpu_enable_all_gather_combine_by_dim=false",
+            "--xla_gpu_enable_reduce_scatter_combine_by_dim=false",
+            "--xla_disable_hlo_passes=rematerialization",
         ]
         env_vars["XLA_FLAGS"] = " ".join(default_xla_flags)
 

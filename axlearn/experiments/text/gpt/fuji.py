dd60993d8473c606ab459be66cbb9587958db808
Sam Stoelinga <sammiestoel@gmail.com>
2024-09-27 17:20:15 -0700

    gbs=64*2

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -260,7 +260,7 @@ def get_trainer_kwargs(
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
             # train_batch_size=train_batch_size,
-            train_batch_size=64,
+            train_batch_size=64 * 2,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(

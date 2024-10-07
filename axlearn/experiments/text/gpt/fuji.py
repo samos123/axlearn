4103a575da449826b6eaa882473a9fb8ad2b2922
Erin Horning <{ID}+{username}@users.noreply.github.com>
2024-10-07 16:34:41 +0000

    up batch size

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -261,8 +261,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            # train_batch_size=train_batch_size,
-            train_batch_size=512,
+            train_batch_size=train_batch_size,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(

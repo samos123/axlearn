1e556b8d207fed78098933efcfc99f3af9df67e4
Sam Stoelinga <sammiestoel@gmail.com>
2024-10-29 11:03:19 -0700

    move checkpoint to middle

--- axlearn/common/decoder.py
+++ axlearn/common/decoder.py
@@ -508,6 +508,7 @@ class Decoder(DecodingMixin, BaseLayer):
         else:
             raise ValueError(f"Unrecognized mode {mode}.")
         x = x.data
+        x = self._remat_name(x, "decoder_input")
         self._add_tensor_stats("outputs", x)
 
         if "output_norm" in self.children:
@@ -558,7 +559,6 @@ class Decoder(DecodingMixin, BaseLayer):
                 logits: A float Tensor of shape [batch_size, target_len, num_classes], where
                     num_classes depends on the configured lm_head.
         """
-        input_ids = self._remat_name(input_ids, "decoder_input")
         _, output = self._forward_for_mode(
             mode=ForwardMode.FORWARD,
             input_ids=input_ids,

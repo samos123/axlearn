3093687e428722f22e1ca2807de0d158bca285c2
Sam Stoelinga <sammiestoel@gmail.com>
2024-10-30 15:48:43 -0700

    move to end of function

--- axlearn/common/embedding.py
+++ axlearn/common/embedding.py
@@ -63,7 +63,6 @@ class TransformerTextEmbeddings(BaseLayer):
             A float Tensor of shape [batch_size, seq_len, hidden_dim]
         """
         x = self.token_emb(inputs)
-        x = self._remat_name(x, "activation_inputs")
         if self.config.type_emb is not None:
             if token_type_ids is None:
                 token_type_ids = jnp.zeros_like(inputs)
@@ -75,6 +74,7 @@ class TransformerTextEmbeddings(BaseLayer):
         if self.config.norm is not None:
             x = self.norm(x)
         x = self.dropout(x)
+        x = self._remat_name(x, "activation_inputs")
         return x
 
     def attend(self, x: Tensor) -> Tensor:

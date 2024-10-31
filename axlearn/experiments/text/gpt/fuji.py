ecced02ec39d6b4b9878993135348bc40d6f10d1
Sam Stoelinga <sammiestoel@gmail.com>
2024-10-30 23:50:04 -0700

    half gbs

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -367,7 +367,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=8e-5, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=512,
+            train_batch_size=128,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(
@@ -377,7 +377,7 @@ def get_trainer_kwargs(
                     ChainConfigModifier.default_config().set(
                         config_modifiers=[
                             MeshShapeModifier.default_config().set(
-                                mesh_shape=mesh_shape_from_axes(fsdp=-1, model=16)
+                                mesh_shape=mesh_shape_from_axes(fsdp=16, model=16)
                             ),
                             RematSpecModifier.default_config().set(
                                 remat_policies={

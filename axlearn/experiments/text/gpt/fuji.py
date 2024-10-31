9680fa88e0486728668a585a8fd4932624581d8d
Sam Stoelinga <sammiestoel@gmail.com>
2024-10-30 23:37:22 -0700

    fsdp=512 gbs=512

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -377,7 +377,7 @@ def get_trainer_kwargs(
                     ChainConfigModifier.default_config().set(
                         config_modifiers=[
                             MeshShapeModifier.default_config().set(
-                                mesh_shape=mesh_shape_from_axes(data=-1, fsdp=64 * 2, model=4)
+                                mesh_shape=mesh_shape_from_axes(fsdp=-1)
                             ),
                             RematSpecModifier.default_config().set(
                                 remat_policies={

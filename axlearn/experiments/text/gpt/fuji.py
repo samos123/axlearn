371ab9ad8b5aa09c16832e48ead704bb769c6134
Sam Stoelinga <sammiestoel@gmail.com>
2024-11-08 10:47:22 -0800

    remove k_proj from offload

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -360,10 +360,10 @@ def get_trainer_kwargs(
             names_which_can_be_saved=[],
             names_which_can_be_offloaded=[
                 "FlashAttention.q_proj",
-                "FlashAttention.k_proj",
+                # "FlashAttention.k_proj",
                 "FlashAttention.v_proj",
                 # "FlashAttention.context",
-                # "FlashAttention.o_proj",
+                "FlashAttention.o_proj",
                 # "decoder_input",
                 # "activation",
                 # "decoder_x_after_emb",

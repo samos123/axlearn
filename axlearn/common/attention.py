7cccb46a2cb97800454c2add501b7f29d36f40bd
Sam Stoelinga <sammiestoel@gmail.com>
2024-11-18 17:29:24 -0800

    revert not needed checkpoints

--- axlearn/common/attention.py
+++ axlearn/common/attention.py
@@ -3170,8 +3170,7 @@ class TransformerLayer(BaseTransformerLayer):
         Raises:
             ValueError: If `mode` is unsupported.
         """
-        # data = self._remat_name(data, "transformer_forward_data")
-        data = jax.ad_checkpoint.checkpoint_name(data, "transformer_forward_data")
+        data = self._remat_name(data, "input")
         self.vlog(3, "transformer.input=%s", data.sum())
         self_attention_return_aux = set()
         cross_attention_return_aux = set()

2bca767b17de3f772d549fe6c606b20c4c7ff7dd
Sam Stoelinga <sammiestoel@gmail.com>
2024-11-19 15:03:52 -0800

    Revert "xla_jf_crs_combiner_threshold_count=2"
    This reverts commit cdd5c7632ef51d945ed2cdf05a0608b3817fe6ce.

--- axlearn/common/compiler_options.py
+++ axlearn/common/compiler_options.py
@@ -94,7 +94,7 @@ def default_xla_options(
             xla_sc_disable_megacore_partitioning="true",
             xla_tpu_use_tc_device_shape_on_sc="true",
             tpu_use_continuations="true",
-            xla_jf_crs_combiner_threshold_count="2",
+            xla_jf_crs_combiner_threshold_count="10",
             xla_sc_enable_instruction_fusion="false",
             xla_sc_disjoint_spmem="false",
             xla_tpu_enable_sparse_core_collective_offload_all_reduce="true",

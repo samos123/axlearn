63c770d1acb05dc92448e2d1880effd679c397be
Sam Stoelinga <sammiestoel@gmail.com>
2025-01-28 15:47:28 -0800

    set 128 gbs statically

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -15,7 +15,7 @@ import functools
 import itertools
 from typing import Any, Optional, Union
 
-import jax
+# import jax
 from jax.ad_checkpoint import checkpoint_policies as jax_remat_policies
 
 from axlearn.common import causal_lm, config
@@ -434,7 +434,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=len(jax.devices()) // 4,
+            train_batch_size=128,
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(

29a217086f2be38aa79da041b2a23d080c91877f
Sam Stoelinga <stoelinga@google.com>
2025-09-10 02:17:04 +0000

    block size 4096

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -735,7 +735,7 @@ def get_trainer_kwargs(
                             MeshShapeModifier.default_config().set(
                                 mesh_shape=mesh_shape_from_axes(fsdp=-1)
                             ),
-                            FlashBlockSizeModifier.default_config().set(tpu_block_size=2048),
+                            FlashBlockSizeModifier.default_config().set(tpu_block_size=4096),
                             RematSpecModifier.default_config().set(
                                 remat_policies={
                                     "model.decoder.transformer.layer": RematSpec(
@@ -941,7 +941,7 @@ def get_trainer_kwargs(
                             MeshShapeModifier.default_config().set(
                                 mesh_shape=mesh_shape_from_axes(fsdp=-1)
                             ),
-                            FlashBlockSizeModifier.default_config().set(tpu_block_size=2048),
+                            FlashBlockSizeModifier.default_config().set(tpu_block_size=4096),
                             RematSpecModifier.default_config().set(
                                 remat_policies={
                                     "model.decoder.transformer.layer": RematSpec(
